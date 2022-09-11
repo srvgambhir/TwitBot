b7b3f0ca4772af89958fcd839a75bbe2fc8feb2e
Sarvesh-local <srvgambhir@gmail.com>
2022-09-11 00:59:09 -0400

    modified clock

--- clock.py
+++ clock.py
@@ -7,7 +7,7 @@ sched = BlockingScheduler()
 # def timed_job():
 #     print('This job is run every three minutes.')
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=0)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1)
 def scheduled_job():
     print('This job is run everyday at midnight.')
     main.tweet()
